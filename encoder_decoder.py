99022eccc7ed0b3b699578980fbd45e090923571
Pryce Houck <prycehh@gmail.com>
2023-10-24 19:27:25 -0400

    decoder function added, main functionality added, main error handling added LGTM

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -13,7 +13,19 @@ def encoder(num):
         password += str(val)
     return password
 
+
+def decoder(num):
+    decoded = ""
+    for i in range(len(num)):
+        temp = int(num[i]) - 3
+        if temp < 0:
+            temp += 10
+        decoded += str(temp)
+    return decoded
+
+
 def main():
+    password = ""
     run = True
     while run:
         print("Menu \n ------------ \n 1.encoder \n 2. decoder \n 3. Quit")
@@ -27,7 +39,17 @@ def main():
                 password = encoder(num)
                 print(password)
                 valid = False
-
+            elif select == 2:
+                if password != '':
+                    print(f'The encoded password is {password}, and the original password is {decoder(password)}')
+                else:
+                    print('No password stored.')
+                valid = False
+            elif select == 3:
+                valid = False
+                run = False
+            else:
+                print('Please enter a valid selection.')
 
 
 if __name__ == '__main__':
