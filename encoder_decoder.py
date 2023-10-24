75ab59239ce4eb812b7d6cfc78f1560a0b359cd1
SarahCedeno <sarahcedeno.12@gmail.com>
2023-10-24 19:15:00 -0400

    wrote the encoder and most of the main menu

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -3,13 +3,30 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+def encoder(num):
+    password = ""
+    val = 0
+    for i in range(0, len(num)):
+        val = int(num[i])+3
+        if val >= 10:
+            val -= 10
+        password += str(val)
+    return password
 
 def main():
     run = True
     while run:
         print("Menu \n ------------ \n 1.encoder \n 2. decoder \n 3. Quit")
+        valid = True
 
+        while valid:
+            select = int(input("Please enter an option:"))
+
+            if select == 1:
+                num = input("Please enter a password to encode:")
+                password = encoder(num)
+                print(password)
+                valid = False
 
 
 
